714bf982babe809abc9700e1bc6d35558d958d4d
Riser97 <119424167+Riser97@users.noreply.github.com>
2022-12-05 21:37:12 +0800

    Create caesars-cipher.js

---
+++ caesars-cipher.js
@@ -0,0 +1,9 @@
+function rot13(str) {
+  var rotCharArray = [];
+  var regEx = /[A-Z]/;
+  str = str.split("");
+  for (var x in str) {
+    if (regEx.test(str[x])) {
+      rotCharArray.push(((str[x].charCodeAt() - 65 + 13) % 26) + 65);
+    } 
+}
